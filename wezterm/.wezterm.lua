71b530ddc1aaa96e76a9e27bd70f2edf08e4d8a5
CyberVarun <varunjagtap424@gmail.com>
2024-08-28 10:06:06 +0530

    Added WezTerm config.

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,33 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+config.font = wezterm.font("MesloLGM Nerd Font Mono")
+config.font_size = 13
+
+config.color_scheme = "Brogrammer (Gogh)"
+
+config.default_prog = { "/bin/tmux", "-u" }
+
+config.initial_rows = 32
+config.initial_cols = 122
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.7
+
+config.keys = {
+	{
+		key = "n",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.ToggleFullScreen,
+	},
+}
+
+-- and finally, return the configuration to wezterm
+return config
